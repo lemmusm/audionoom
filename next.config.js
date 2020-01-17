719e288ae67667585f11e7e984312b0b8b5427f4
bettyrjc <bettyjimenez3010@gmail.com>
2020-01-17 19:36:06 -0400

    añadiendo

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 const withSass = require('@zeit/next-sass')
 const withImages = require('next-images')
-module.exports = withImages()
+module.exports = withImages({
+  inlineImageLimit: 16384,
+  webpack(config, options) {
+    return config
+  }
+})
 module.exports = withSass({
   cssModules: true,
   cssLoaderOptions: {
