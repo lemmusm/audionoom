eb23a7df6549c3c8d93c98522149b4f3e25ac1c0
lemmusm <lemmusm@gmail.com>
2020-01-17 21:31:53 -0600

    Fix next.config.js and update .gitignore

--- components/common/Header.js
+++ components/common/Header.js
@@ -1,39 +1,35 @@
-import React, { Component } from 'react'
-import {Link} from '../../routes'
-import Head from 'next/head'
-import css from './styles.scss'
-// import img from '../../public/cuadro_verde.png'
-import {image} from '../../public/image'
+import React, { Component } from 'react';
+import { Link } from '../../routes';
+import Head from 'next/head';
+import css from './styles.scss';
+import img from '../../public/cuadro_verde.png';
+import { image } from '../../public/image';
 
- export default class Header extends Component {
-   render() {
-     const {title} = this.props
-     return (
-       <div>
+export default class Header extends Component {
+  render() {
+    console.log(this.props);
+    const { title } = this.props;
+    return (
+      <div>
         <Head>
-          <title>{ title }</title>
-          <meta name="viewport" content="width=device-width"/>
+          <title>{title}</title>
+          <meta name='viewport' content='width=device-width' />
         </Head>
         <header className={css.header}>
           <div className={css.logo}>
-            <img src={image}
-                alt='logo'
-            />
+            <img src={img} alt='logo' />
           </div>
           <ul className={css.items}>
             <li className={css.item}>Hola</li>
             <li className={css.item}>Hola 2</li>
             <li className={css.item}>
               <Link route='prueba'>
-                <a>
-                  Prueba
-                </a>
+                <a>Prueba</a>
               </Link>
             </li>
           </ul>
         </header>
       </div>
-     )
-   }
- }
- 
+    );
+  }
+}
